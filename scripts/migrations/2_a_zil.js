3cebc05ff73ba87334060e74641e97c1c4ec64e2
Nikolay Sverchkov <ssnikolay@gmail.com>
2021-11-11 19:11:23 +0400

    Delegate and ReDeletage work via aZIL + Buffer

--- scripts/migrations/2_a_zil.js
+++ scripts/migrations/2_a_zil.js
@@ -5,15 +5,25 @@ const Contracts = require('../utils/contracts.js');
   try {
     const extendedInit = [
       {
-        vname: 'azil_ssn_addrress',
+        vname: 'azil_ssn_address',
         type: 'ByStr20',
         value: '0x166862bdd5d76b3a4775d2494820179d582acac5'
       },
       {
-        vname: 'proxy_staking_contract_address',
+        vname: 'init_proxy_staking_contract_address',
         type: 'ByStr20',
         value: '0xb2e2c996e6068f4ae11c4cc2c6a189b774819f79'
       },
+      {
+        vname: 'init_buffer_address',
+        type: 'ByStr20',
+        value: '0xb2e2c996e6068f4ae11c4cc2c6a189b774819f79'
+      },
+      {
+        vname: 'init_holder_address',
+        type: 'ByStr20',
+        value: '0xb2e2c996e6068f4ae11c4cc2c6a189b774819f79'
+      }
     ];
 
     const AZil = await Contracts.newFromFile('aZil', extendedInit);
