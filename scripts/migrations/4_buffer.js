dd297dd545c88a99789b7fffd6c9a2e900987246
Nikolay Sverchkov <ssnikolay@gmail.com>
2021-11-12 16:49:27 +0400

    Move ssn and proxy address to holder & buffer level

--- scripts/migrations/4_buffer.js
+++ scripts/migrations/4_buffer.js
@@ -3,7 +3,20 @@ const Contracts = require('../utils/contracts.js');
 
 (async() => {
   try {
-    const Buffer = await Contracts.newFromFile('buffer', []);
+    const extendedInit = [
+      {
+        vname: 'init_azil_ssn_address',
+        type: 'ByStr20',
+        value: '0x166862bdd5d76b3a4775d2494820179d582acac5'
+      },
+      {
+        vname: 'init_proxy_staking_contract_address',
+        type: 'ByStr20',
+        value: '0xb2e2c996e6068f4ae11c4cc2c6a189b774819f79'
+      }
+    ];
+
+    const Buffer = await Contracts.newFromFile('buffer', extendedInit);
 
     await Deployer.deploy(Buffer);
 
