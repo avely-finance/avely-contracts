2cbc5b5f0fc7681e3a8fe461b6533a19d63ecda0
Max Hiker <maxhiker@pm.me>
2021-12-08 15:33:01 +0200

    moved TxLastId var to Contract globals; tests fixed

--- tests/deploy/buffer.go
+++ tests/deploy/buffer.go
@@ -114,14 +114,16 @@ func NewBufferContract(key string, aimplAddress string, aZilSSNAddress string, s
 	tx.Confirm(tx.ID, TxConfirmMaxAttempts, TxConfirmInterval, contract.Provider)
 	if tx.Status == core.Confirmed {
 		b32, _ := bech32.ToBech32Address(tx.ContractAddress)
+		stateFieldTypes := make(StateFieldTypes)
 		contract := Contract{
-			Code:     string(code),
-			Init:     init,
-			Addr:     tx.ContractAddress,
-			Bech32:   b32,
-			Wallet:   wallet,
-			TxIdLast: tx.ID,
+			Code:            string(code),
+			Init:            init,
+			Addr:            tx.ContractAddress,
+			Bech32:          b32,
+			Wallet:          wallet,
+			StateFieldTypes: stateFieldTypes,
 		}
+		TxIdLast = tx.ID
 		return &BufferContract{Contract: contract}, nil
 	} else {
 		data, _ := json.MarshalIndent(tx.Receipt, "", "     ")
