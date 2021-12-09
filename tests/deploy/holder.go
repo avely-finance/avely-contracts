d46035ba8cea9a7c05bafcc377c239e97c695daf
Max Hiker <maxhiker@pm.me>
2021-12-09 12:14:46 +0200

    Added Holder->IsZimpl(); callbacks protected; tests updated; var names shortened

--- tests/deploy/holder.go
+++ tests/deploy/holder.go
@@ -17,7 +17,7 @@ type HolderContract struct {
 	Contract
 }
 
-func (b *HolderContract) ChangeProxyStakingContractAddress(new_addr string) (*transaction.Transaction, error) {
+func (b *HolderContract) ChangeZproxyAddress(new_addr string) (*transaction.Transaction, error) {
 	args := []core.ContractValue{
 		{
 			"address",
@@ -25,7 +25,18 @@ func (b *HolderContract) ChangeProxyStakingContractAddress(new_addr string) (*tr
 			"0x" + new_addr,
 		},
 	}
-	return b.Call("ChangeProxyStakingContractAddress", args, "0")
+	return b.Call("ChangeZproxyAddress", args, "0")
+}
+
+func (b *HolderContract) ChangeZimplAddress(new_addr string) (*transaction.Transaction, error) {
+	args := []core.ContractValue{
+		{
+			"address",
+			"ByStr20",
+			"0x" + new_addr,
+		},
+	}
+	return b.Call("ChangeZimplAddress", args, "0")
 }
 
 func (b *HolderContract) ChangeAzilSSNAddress(new_addr string) (*transaction.Transaction, error) {
@@ -92,7 +103,11 @@ func NewHolderContract(key string, aimplAddress string, aZilSSNAddress string, s
 			Type:  "ByStr20",
 			Value: aZilSSNAddress,
 		}, {
-			VName: "init_proxy_staking_contract_address",
+			VName: "init_zproxy_address",
+			Type:  "ByStr20",
+			Value: "0x" + stubStakingAddr,
+		}, {
+			VName: "init_zimpl_address",
 			Type:  "ByStr20",
 			Value: "0x" + stubStakingAddr,
 		},
