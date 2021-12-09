59ad1ffa4fec0a6852919c1927639c120dd79c1b
Max Hiker <maxhiker@pm.me>
2021-12-09 12:25:03 +0200

    Added tests for IsImpl(holder)

--- tests/transitions/is_zimpl.go
+++ tests/transitions/is_zimpl.go
@@ -4,7 +4,7 @@ func (t *Testing) IsZimpl() {
 
     t.LogStart("IsAimpl")
 
-    _, _, bufferContract, _ := t.DeployAndUpgrade()
+    _, _, bufferContract, holderContract := t.DeployAndUpgrade()
 
     // Use random user for Buffer
     bufferContract.UpdateWallet(key2)
@@ -16,4 +16,16 @@ func (t *Testing) IsZimpl() {
     tx, err = bufferContract.DelegateStakeSuccessCallBack(addr2, zil(10))
     t.AssertError(tx, err, -407)
 
+    // Use random user for Buffer
+    holderContract.UpdateWallet(key2)
+    tx, err = holderContract.AddFunds(zil(10))
+    t.AssertError(tx, err, -307)
+    tx, err = holderContract.WithdrawStakeAmtSuccessCallBack(addr2, zil(10))
+    t.AssertError(tx, err, -307)
+    tx, err = holderContract.WithdrawStakeRewardsSuccessCallBack(addr2, zil(10))
+    t.AssertError(tx, err, -307)
+    tx, err = holderContract.CompleteWithdrawalSuccessCallBack(zil(10))
+    t.AssertError(tx, err, -307)
+    tx, err = holderContract.CompleteWithdrawalNoUnbondedStakeCallBack(zil(10))
+    t.AssertError(tx, err, -307)
 }
