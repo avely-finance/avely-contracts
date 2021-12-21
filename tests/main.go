5733a6635bc3386499f0f73b1223d3ffcfaefd18
Nikolay Sverchkov <ssnikolay@gmail.com>
2021-12-21 18:07:43 +0300

    Add focus on test suite

--- tests/main.go
+++ tests/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"Azil/test/helpers"
 	"Azil/test/transitions"
+	"flag"
+	"reflect"
 	"time"
 )
 
@@ -33,6 +35,32 @@ func main() {
 
 	go increaseBlocknum()
 	tr := transitions.NewTransitions(config)
+
+	// Example: go run main.go -focus=DelegateStakeSuccess
+	focusPtr := flag.String("focus", "default", "a focus test suite")
+
+	flag.Parse()
+
+	focus := string(*focusPtr)
+
+	if focus != "default" {
+		log.Info("🏁 Focus on " + focus)
+		st := reflect.TypeOf(tr)
+		_, exists := st.MethodByName(focus)
+		if exists {
+			reflect.ValueOf(tr).MethodByName(focus).Call([]reflect.Value{})
+		} else {
+			log.Fatal(" A focus test suite does not exist")
+		}
+	} else {
+		log.Info("🏁 Run All Suites ")
+		runAll(tr)
+	}
+
+	log.Info("🏁 TESTS PASSED SUCCESSFULLY")
+}
+
+func runAll(tr *transitions.Transitions) {
 	tr.DelegateStakeSuccess()
 	tr.DelegateStakeBuffersRotation()
 	tr.WithdrawStakeAmount()
@@ -43,5 +71,4 @@ func main() {
 	tr.IsZimpl()
 	tr.DrainBuffer()
 	tr.PerformAuoRestake()
-	log.Info("🏁 TESTS PASSED SUCCESSFULLY")
 }
