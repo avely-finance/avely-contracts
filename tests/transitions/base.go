ac3d01884f67da0a7ffcbfe84150d4d682e8674d
Nikolay Sverchkov <ssnikolay@gmail.com>
2021-12-22 10:41:49 +0300

    Move focus run logic to transitions

--- tests/transitions/base.go
+++ tests/transitions/base.go
@@ -2,6 +2,7 @@ package transitions
 
 import (
 	. "Azil/test/helpers"
+	"reflect"
 )
 
 var t *Testing
@@ -20,4 +21,27 @@ func NewTransitions(config Config) *Transitions {
 	return &Transitions{
 		cfg: config,
 	}
+}
+
+func (tr *Transitions) FocusOn(focus string) {
+	st := reflect.TypeOf(tr)
+	_, exists := st.MethodByName(focus)
+	if exists {
+		reflect.ValueOf(tr).MethodByName(focus).Call([]reflect.Value{})
+	} else {
+		log.Fatal(" A focus test suite does not exist")
+	}
+}
+
+func (tr *Transitions) RunAll() {
+	tr.DelegateStakeSuccess()
+	tr.DelegateStakeBuffersRotation()
+	tr.WithdrawStakeAmount()
+	tr.CompleteWithdrawalSuccess()
+	tr.ZilBalanceOf()
+	tr.IsAdmin()
+	tr.IsAimpl()
+	tr.IsZimpl()
+	tr.DrainBuffer()
+	tr.PerformAuoRestake()
 }
