62b656d1351f01366d57c69bc456126417070c91
Max Hiker <maxhiker@pm.me>
2022-01-01 15:32:39 +0200

    Added Aproxy->UpgradeTo() transition; SDK updated; Proxy() test suite created

---
+++ tests/transitions/proxy.go
@@ -0,0 +1,26 @@
+package transitions
+
+import (
+    //. "github.com/avely-finance/avely-contracts/sdk/utils"
+    . "github.com/avely-finance/avely-contracts/tests/helpers"
+)
+
+func (tr *Transitions) Proxy() {
+
+    Start("Proxy")
+
+    p := tr.DeployAndUpgrade()
+
+    newImpl := "0x" + sdk.Cfg.Addr3
+
+    //non-admin
+    p.Aproxy.UpdateWallet(sdk.Cfg.Key3)
+    tx, err := p.Aproxy.UpgradeTo(newImpl)
+    AssertError(tx, err, -202)
+
+    //admin
+    p.Aproxy.UpdateWallet(sdk.Cfg.AdminKey)
+    tx, _ = AssertSuccess(p.Aproxy.UpgradeTo(newImpl))
+    AssertEvent(tx, Event{p.Aproxy.Addr, "UpgradeTo", ParamsMap{"aimpl_address": newImpl}})
+    AssertEqual(p.Aproxy.Field("aimpl_address"), newImpl)
+}
