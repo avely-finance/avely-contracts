018bb4b1eb778d584e56b49a1794b4208f364d9b
Max Hiker <maxhiker@pm.me>
2022-01-02 19:51:36 +0200

    testsuite reorganized

--- tests/transitions/proxy.go
+++ tests/transitions/proxy.go
@@ -1,6 +1,7 @@
 package transitions
 
 import (
+    "github.com/avely-finance/avely-contracts/sdk/contracts"
     . "github.com/avely-finance/avely-contracts/sdk/utils"
     . "github.com/avely-finance/avely-contracts/tests/helpers"
 )
@@ -11,29 +12,41 @@ func (tr *Transitions) Proxy() {
 
     p := tr.DeployAndUpgrade()
 
-    newAddr := sdk.Cfg.Addr3
+    callAimplDirectly(p)
+    callNonAdmin(p)
 
-    //call proxy admin transitions with non-admin user; expecting errors
-    p.Aproxy.UpdateWallet(sdk.Cfg.Key3)
-    tx, err := p.Aproxy.UpgradeTo(newAddr)
-    AssertError(tx, err, -202)
+    newAdminAddr := sdk.Cfg.Addr3
+    newAdminKey := sdk.Cfg.Key3
+    newImplAddr := "0000000000000000000000000000000000000000"
 
-    tx, err = p.Aproxy.ChangeAdmin(newAddr)
-    AssertError(tx, err, -202)
-
-    //call proxy admin transitions with admin user; expecting success
+    //change admin, expecting success
     p.Aproxy.UpdateWallet(sdk.Cfg.AdminKey)
-    tx, _ = AssertSuccess(p.Aproxy.UpgradeTo(newAddr))
-    AssertEvent(tx, Event{p.Aproxy.Addr, "UpgradeTo", ParamsMap{"aimpl_address": "0x" + newAddr}})
-    AssertEqual(p.Aproxy.Field("aimpl_address"), "0x"+newAddr)
+    tx, _ := AssertSuccess(p.Aproxy.ChangeAdmin(newAdminAddr))
+    AssertEvent(tx, Event{p.Aproxy.Addr, "ChangeAdmin", ParamsMap{"currentAdmin": "0x" + sdk.Cfg.Admin, "newAdmin": "0x" + newAdminAddr}})
+    AssertEqual(p.Aproxy.Field("staging_admin_address"), "0x"+newAdminAddr)
 
-    tx, _ = AssertSuccess(p.Aproxy.ChangeAdmin(newAddr))
-    AssertEvent(tx, Event{p.Aproxy.Addr, "ChangeAdmin", ParamsMap{"currentAdmin": "0x" + sdk.Cfg.Admin, "newAdmin": "0x" + newAddr}})
-    AssertEqual(p.Aproxy.Field("staging_admin_address"), "0x"+newAddr)
+    //claim admin with wrong user, expecting error
+    wrongActor := sdk.Cfg.Key1
+    p.Aproxy.UpdateWallet(wrongActor)
+    tx, err := p.Aproxy.ClaimAdmin()
+    AssertError(tx, err, -203)
 
+    //claim admin with correct user, expecting success
+    p.Aproxy.UpdateWallet(newAdminKey)
+    tx, _ = AssertSuccess(p.Aproxy.ClaimAdmin())
+    AssertEvent(tx, Event{p.Aproxy.Addr, "ClaimAdmin", ParamsMap{"newAdmin": "0x" + newAdminAddr}})
+    AssertEqual(p.Aproxy.Field("admin_address"), "0x"+newAdminAddr)
+
+    //call UpgradeTo with new admin, expecting success
+    tx, _ = AssertSuccess(p.Aproxy.UpgradeTo(newImplAddr))
+    AssertEvent(tx, Event{p.Aproxy.Addr, "UpgradeTo", ParamsMap{"aimpl_address": "0x" + newImplAddr}})
+    AssertEqual(p.Aproxy.Field("aimpl_address"), "0x"+newImplAddr)
+}
+
+func callAimplDirectly(p *contracts.Protocol) {
     //call aimpl transitions, which are supposed to call through proxy, directly; expecting errors
     initiator := sdk.Cfg.Addr3
-    tx, err = p.Aimpl.DelegateStake(ToZil(10), initiator)
+    tx, err := p.Aimpl.DelegateStake(ToZil(10), initiator)
     AssertError(tx, err, -113)
 
     tx, err = p.Aimpl.ZilBalanceOf(initiator, initiator)
@@ -44,17 +57,14 @@ func (tr *Transitions) Proxy() {
 
     tx, err = p.Aimpl.CompleteWithdrawal(initiator)
     AssertError(tx, err, -113)
+}
 
-    //claim admin
-    wrongActor := sdk.Cfg.Key1
-    p.Aproxy.UpdateWallet(wrongActor)
-    tx, err = p.Aproxy.ClaimAdmin()
-    AssertError(tx, err, -203)
+func callNonAdmin(p *contracts.Protocol) {
+    //call proxy admin transitions with non-admin user; expecting errors
+    p.Aproxy.UpdateWallet(sdk.Cfg.Key1)
+    tx, err := p.Aproxy.UpgradeTo("0000000000000000000000000000000000000000")
+    AssertError(tx, err, -202)
 
-    newAdmin := sdk.Cfg.Key3
-    p.Aproxy.UpdateWallet(newAdmin)
-    tx, _ = AssertSuccess(p.Aproxy.ClaimAdmin())
-    AssertEvent(tx, Event{p.Aproxy.Addr, "ClaimAdmin", ParamsMap{"newAdmin": "0x" + newAddr}})
-    AssertEqual(p.Aproxy.Field("admin_address"), "0x"+newAddr)
-
+    tx, err = p.Aproxy.ChangeAdmin("0000000000000000000000000000000000000000")
+    AssertError(tx, err, -202)
 }
