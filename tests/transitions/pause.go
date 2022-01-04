8b827c5c37fc550124380e8432f3b65a12087bf9
Max Hiker <maxhiker@pm.me>
2022-01-04 17:46:39 +0200

    tests finished

--- tests/transitions/pause.go
+++ tests/transitions/pause.go
@@ -7,15 +7,28 @@ import (
 )
 
 func (tr *Transitions) Pause() {
-	Start("IsAimpl")
+
+	Start("Pause/Unpause")
 
 	p := tr.DeployAndUpgrade()
 
 	callPaused(p)
-	callPauseNonAdmin(p)
+	callPauseUnpauseNonAdmin(p)
+
+	p.Aimpl.UpdateWallet(sdk.Cfg.AdminKey)
+
+	//pause contract, expecting success
+	tx, _ := AssertSuccess(p.Aimpl.Pause())
+	AssertEvent(tx, Event{p.Aimpl.Addr, "Pause", ParamsMap{"is_paused": "1"}})
+	AssertEqual(p.Aimpl.Field("is_paused"), "True")
+
+	//unpause contract, expecting success
+	tx, _ = AssertSuccess(p.Aimpl.Unpause())
+	AssertEvent(tx, Event{p.Aimpl.Addr, "UnPause", ParamsMap{"is_paused": "0"}})
+	AssertEqual(p.Aimpl.Field("is_paused"), "False")
 }
 
-func callPauseNonAdmin(p *contracts.Protocol) {
+func callPauseUnpauseNonAdmin(p *contracts.Protocol) {
 	//call pause/unpause admin transitions with non-admin user; expecting errors
 	p.Aimpl.UpdateWallet(sdk.Cfg.Key1)
 
@@ -30,7 +43,10 @@ func callPaused(p *contracts.Protocol) {
 	//call user's transitions, when contract is paused; expecting errors
 	AssertSuccess(p.Aimpl.Pause())
 
-	tx, _ := p.Aproxy.DelegateStake(ToZil(10))
+	tx, _ := p.Aimpl.Pause()
+	AssertError(tx, "Paused")
+
+	tx, _ = p.Aproxy.DelegateStake(ToZil(10))
 	AssertError(tx, "Paused")
 
 	p.Aproxy.ZilBalanceOf(sdk.Cfg.Addr1)
