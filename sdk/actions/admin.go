a89b90584279d8e6b08b8b88be739b0928bb547e
Max Hiker <maxhiker@pm.me>
2022-01-29 12:42:46 +0100

    refactor actions/admin->ChownStakeReDelegate()

--- sdk/actions/admin.go
+++ sdk/actions/admin.go
@@ -1,56 +1,55 @@
 package actions
 
 import (
-    . "github.com/avely-finance/avely-contracts/sdk/contracts"
-    "github.com/avely-finance/avely-contracts/tests/helpers"
-    "strings"
+	. "github.com/avely-finance/avely-contracts/sdk/contracts"
+	"github.com/avely-finance/avely-contracts/tests/helpers"
 )
 
 var log = helpers.GetLog()
 
 func ChownStakeReDelegate(p *Protocol) {
-    activeBuffer := p.GetActiveBuffer()
-    aZilSsnAddr := p.GetAzilSsnAddress()
+	activeBuffer := p.GetActiveBuffer()
+	aZilSsnAddr := p.GetAzilSsnAddress()
 
-    log.Success("Active Buffer is " + activeBuffer.Addr)
-    addr := strings.ToLower(activeBuffer.Addr)
-    deposits, _ := p.Zimpl.GetDeposiAmtDeleg(addr)
+	log.Successf("Active Buffer is %s", activeBuffer.Addr)
 
-    if value, found := deposits[addr]; found {
-        depositsMap := value.Map()
-        for ssn, amount := range depositsMap {
-            if ssn != aZilSsnAddr {
-                tx, err := p.Aimpl.ChownStakeReDelegate(ssn, amount.String())
+	mapSsnAmount := p.Zimpl.GetDepositAmtDeleg(activeBuffer.Addr)
+	if 0 == len(mapSsnAmount) {
+		log.Success("Buffer is empty, nothing to redelegate.")
+		return
+	}
 
-                if err != nil {
-                    log.Fatal("ChownStakeReDelegate is failed. Tx: " + tx.ID)
-                } else {
-                    log.Success("Successfully redelegate " + amount.String() + " from SSN " + ssn + "; Tx: " + tx.ID)
-                }
-            }
+	for ssn, amount := range mapSsnAmount {
+		amountStr := amount.String()
+		if ssn != aZilSsnAddr {
+			tx, err := p.Aimpl.ChownStakeReDelegate(ssn, amountStr)
+			if err != nil {
+				log.Fatalf("ChownStakeReDelegate(%s, %s) ERROR. Tx: %s.", ssn, amountStr, tx.ID)
+			} else {
+				log.Successf("ChownStakeReDelegate(%s, %s) OK, Tx: %s.", ssn, amountStr, tx.ID)
+			}
+		} else {
+			log.Successf("AzilSSN %s has %s. Skip ChownStakeReDelegate.", ssn, amountStr)
+		}
+	}
 
-            log.Success(ssn + " has " + amount.String())
-        }
-    } else {
-        log.Success("Buffer is empty")
-    }
 }
 
 func ConfirmSwapRequests(p *Protocol) {
-    nextBuffer := p.GetBufferToSwapWith().Addr
-    swapRequests := p.GetSwapRequestsForBuffer(nextBuffer)
-    log.Infof("Found %d swap requests for next buffer %s", len(swapRequests), nextBuffer)
-    errCnt := 0
-    okCnt := 0
-    for _, initiator := range swapRequests {
-        tx, err := p.Aimpl.ChownStakeConfirmSwap(initiator)
-        if err != nil {
-            log.Errorf("Can't confirm swap: ChownStakeConfirmSwap(%s) error=%s, txid=%s", initiator, err, tx.ID)
-            errCnt++
-        } else {
-            log.Successf("Swap confirmed: ChownStakeConfirmSwap(%s) OK, txid=%s", initiator, tx.ID)
-            okCnt++
-        }
-    }
-    log.Infof("confirmSwapRequests completed, %d swaps confirmed, %d errors", okCnt, errCnt)
+	nextBuffer := p.GetBufferToSwapWith().Addr
+	swapRequests := p.GetSwapRequestsForBuffer(nextBuffer)
+	log.Infof("Found %d swap requests for next buffer %s", len(swapRequests), nextBuffer)
+	errCnt := 0
+	okCnt := 0
+	for _, initiator := range swapRequests {
+		tx, err := p.Aimpl.ChownStakeConfirmSwap(initiator)
+		if err != nil {
+			log.Errorf("Can't confirm swap: ChownStakeConfirmSwap(%s) error=%s, txid=%s", initiator, err, tx.ID)
+			errCnt++
+		} else {
+			log.Successf("Swap confirmed: ChownStakeConfirmSwap(%s) OK, txid=%s", initiator, tx.ID)
+			okCnt++
+		}
+	}
+	log.Infof("confirmSwapRequests completed, %d swaps confirmed, %d errors", okCnt, errCnt)
 }
