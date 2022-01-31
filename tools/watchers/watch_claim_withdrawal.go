83a8dcef9d234a6af3b5ce941cc43877ef320e66
Max Hiker <maxhiker@pm.me>
2022-01-31 18:20:07 +0100

    feat(tools): add claim withdrawal watcher

---
+++ tools/watchers/watch_claim_withdrawal.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"github.com/avely-finance/avely-contracts/sdk/actions"
+	"github.com/avely-finance/avely-contracts/sdk/contracts"
+	"github.com/avely-finance/avely-contracts/sdk/core"
+	"github.com/avely-finance/avely-contracts/sdk/utils"
+)
+
+type ClaimWithdrawalWatcher struct {
+	gap        int
+	runAtBlock int
+}
+
+var log *core.Log
+var sdk *core.AvelySDK
+var protocol *contracts.Protocol
+
+func main() {
+
+	chainPtr := flag.String("chain", "local", "chain")
+	gapPtr := flag.Int("gap", 5, "gap between blocks")
+
+	flag.Parse()
+
+	config := core.NewConfig(*chainPtr)
+	sdk = core.NewAvelySDK(*config)
+	log = core.NewLog()
+	protocol = contracts.RestoreFromState(sdk, log)
+	url := sdk.GetWsURL()
+
+	claimWatcher := &ClaimWithdrawalWatcher{
+		gap:        *gapPtr,
+		runAtBlock: -1,
+	}
+
+	blockWatcher := utils.CreateBlockWatcher(url)
+	blockWatcher.AddObserver(claimWatcher)
+	blockWatcher.Start()
+}
+
+func (cww *ClaimWithdrawalWatcher) Notify(blockNum int) {
+	if (blockNum - cww.runAtBlock) > cww.gap {
+		log.Successf("Mined block #%d.", blockNum)
+		actions.ClaimWithdrawal(protocol)
+		cww.runAtBlock = blockNum
+	} else {
+		log.Successf("Mined block #%d, but gap=%d <= %d, skip.", blockNum, (blockNum - cww.runAtBlock), cww.gap)
+	}
+}
