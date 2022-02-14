c5fe3ca81b64a89448c8e294e5833d340db31be9
Nikolay Sverchkov <ssnikolay@gmail.com>
2022-02-14 16:08:09 +0300

    Use Icons only for test logs

--- tests/helpers/logger.go
+++ tests/helpers/logger.go
@@ -2,14 +2,47 @@ package helpers
 
 import (
 	sdk "github.com/avely-finance/avely-contracts/sdk/core"
+	"github.com/sirupsen/logrus"
 )
 
 var log *sdk.Log
 
 func init() {
 	log = sdk.NewLog()
+	addColorIcons(log)
+	disableTimestamps(log)
 }
 
 func GetLog() *sdk.Log {
 	return log
+}
+
+type ColorIconHook struct {
+}
+
+func (hook *ColorIconHook) Fire(entry *logrus.Entry) error {
+	switch entry.Level {
+	case logrus.InfoLevel:
+		entry.Message = "🟢 " + entry.Message
+	case logrus.FatalLevel:
+		entry.Message = "💔 " + entry.Message
+	case logrus.ErrorLevel:
+		entry.Message = "🔴 " + entry.Message
+	}
+	return nil
+}
+
+func (hook *ColorIconHook) Levels() []logrus.Level {
+	return []logrus.Level{logrus.FatalLevel, logrus.ErrorLevel, logrus.InfoLevel, logrus.DebugLevel}
+}
+
+func addColorIcons(log *sdk.Log) {
+	colorIconHook := new(ColorIconHook)
+	log.AddHook(colorIconHook)
+}
+
+func disableTimestamps(log *sdk.Log) {
+	log.SetFormatter(&logrus.TextFormatter{
+		DisableTimestamp: true,
+	})
 }
