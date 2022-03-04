d7821a24bda502b1d5bcc25d44aec79dc936edcf
Max Hiker <maxhiker@pm.me>
2022-03-04 09:26:27 +0000

    text: spaces -> tabs

--- tests/transitions/is_owner.go
+++ tests/transitions/is_owner.go
@@ -1,23 +1,23 @@
 package transitions
 
 import (
-    //. "github.com/avely-finance/avely-contracts/sdk/utils"
-    . "github.com/avely-finance/avely-contracts/tests/helpers"
+	//. "github.com/avely-finance/avely-contracts/sdk/utils"
+	. "github.com/avely-finance/avely-contracts/tests/helpers"
 )
 
 func (tr *Transitions) IsOwner() {
 
-    Start("IsOwner")
+	Start("IsOwner")
 
-    p := tr.DeployAndUpgrade()
+	p := tr.DeployAndUpgrade()
 
-    // Use non-owner user for Azil, expecting errors
-    p.Azil.UpdateWallet(sdk.Cfg.Key2)
+	// Use non-owner user for Azil, expecting errors
+	p.Azil.UpdateWallet(sdk.Cfg.Key2)
 
-    tx, _ := p.Azil.ChangeAzilSSNAddress(sdk.Cfg.Addr3)
-    AssertError(tx, "OwnerValidationFailed")
+	tx, _ := p.Azil.ChangeAzilSSNAddress(sdk.Cfg.Addr3)
+	AssertError(tx, "OwnerValidationFailed")
 
-    tx, _ = p.Azil.ChangeTreasuryAddress(sdk.Cfg.Addr3)
-    AssertError(tx, "OwnerValidationFailed")
+	tx, _ = p.Azil.ChangeTreasuryAddress(sdk.Cfg.Addr3)
+	AssertError(tx, "OwnerValidationFailed")
 
 }
