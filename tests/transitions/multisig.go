0d67331cb8163a43d838ac35904ffdc535485f48
Nikolay Sverchkov <ssnikolay@gmail.com>
2022-03-04 13:22:13 +0400

    Add multisig assertions

--- tests/transitions/multisig.go
+++ tests/transitions/multisig.go
@@ -7,11 +7,29 @@ import (
 func (tr *Transitions) MultisigWalletTests() {
 	Start("MultisigWalletTests contract transitions")
 
+	admin := sdk.Cfg.AdminKey
+	owner1 := sdk.Cfg.Key1
+	owner2 := sdk.Cfg.Key2
+
 	owners := []string{sdk.Cfg.Addr1, sdk.Cfg.Addr2}
-	signCount := 1
+	signCount := 2
 	multisig := tr.DeployMultisigWallet(owners, signCount)
 
 	address := sdk.Cfg.Admin // as random address
 
-	AssertSuccess(multisig.SubmitChangeAzilSSNAddressTransaction(address, address))
+	// after submitting transaction it automatically signed by the _sender
+	AssertMultisigSuccess(multisig.WithUser(owner1).SubmitChangeAzilSSNAddressTransaction(address, address))
+
+	txId := 0 // the test transition should be the first
+
+	tx, _ := multisig.WithUser(admin).SignTransaction(txId)
+	AssertMultisigError(tx, "-1") // NonOwnerCannotSign
+
+	tx, _ = multisig.WithUser(owner2).SignTransaction(txId + 1)
+	AssertMultisigError(tx, "-2") // UnknownTransactionId
+
+	AssertMultisigSuccess(multisig.WithUser(owner2).SignTransaction(txId))
+	AssertMultisigSuccess(multisig.WithUser(owner2).SubmitChangeAzilSSNAddressTransaction(address, address))
+
+	// GetLog().Info(tx)
 }
